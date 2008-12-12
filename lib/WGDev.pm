504568fdccc2df726af61ee2d15df04d8759a381
Graham Knop <haarg@haarg.org>
2008-12-11 18:11:39 -0600

    Make WGDev pass Perl::Critic tests

--- lib/WGDev.pm
+++ lib/WGDev.pm
@@ -5,14 +5,14 @@ use warnings;
 our $VERSION = '0.1.0';
 
 use File::Spec ();
-use Cwd ();
+use Cwd        ();
 use Carp qw(croak);
 
 sub new {
-    my $class = shift;
-    my ($root, $config) = @_;
+    my ( $class, $root, $config ) = @_;
     my $self = bless {}, $class;
-    if (-e $config) {
+    if ( $config && -e $config ) {
+
         # file exists as is, save absolute path
         $self->{config_file} = $config = File::Spec->rel2abs($config);
     }
@@ -20,41 +20,50 @@ sub new {
         $self->{root} = $root;
     }
     else {
-        if (-e $config) {
-            my $config_dir = File::Spec->catpath( (File::Spec->splitpath($config))[0,1] );
-            $self->{root} = Cwd::realpath(File::Spec->catdir($config_dir, File::Spec->updir));
+        if ( $config && -e $config ) {
+            my $config_dir = File::Spec->catpath(
+                ( File::Spec->splitpath($config) )[ 0, 1 ] );
+            $self->{root} = Cwd::realpath(
+                File::Spec->catdir( $config_dir, File::Spec->updir ) );
         }
         else {
             my $dir = Cwd::getcwd();
             while (1) {
-                if (-e File::Spec->catfile($dir, 'etc', 'WebGUI.conf.original')) {
+                if (
+                    -e File::Spec->catfile(
+                        $dir, 'etc', 'WebGUI.conf.original'
+                    ) )
+                {
                     $self->{root} = $dir;
                     last;
                 }
-                my $parent = Cwd::realpath( File::Spec->catdir( $dir, File::Spec->updir ) );
+                my $parent = Cwd::realpath(
+                    File::Spec->catdir( $dir, File::Spec->updir ) );
                 last
                     if $dir eq $parent;
                 $dir = $parent;
             }
         }
     }
-    if ($self->{root}) {
-        if (!$config) {
-            opendir my $dh, File::Spec->catdir($self->{root}, 'etc');
+    if ( $self->{root} ) {
+        if ( !$config ) {
+            opendir my $dh, File::Spec->catdir( $self->{root}, 'etc' );
             my @configs = readdir $dh;
             closedir $dh;
-            @configs = grep { /\.conf$/ && ! /^(?:spectre|log).conf$/ } @configs;
-            if (@configs == 1) {
+            @configs = grep { /\.conf$/msx && !/^(?:spectre|log).conf$/msx }
+                @configs;
+            if ( @configs == 1 ) {
                 $config = $configs[0];
             }
         }
         if ($config) {
-            $self->{config_file} ||= File::Spec->catfile($self->{root}, 'etc', $config);
+            $self->{config_file}
+                ||= File::Spec->catfile( $self->{root}, 'etc', $config );
         }
-        $self->{lib}    = File::Spec->catdir($self->{root}, 'lib');
+        $self->{lib} = File::Spec->catdir( $self->{root}, 'lib' );
     }
     else {
-        croak "unable to determine webgui root!";
+        croak 'unable to determine webgui root!';
     }
 
     $self->set_environment;
@@ -68,58 +77,67 @@ sub set_environment {
     $ENV{WEBGUI_CONFIG} = $self->config_file;
     unshift @INC, $self->lib;
     $ENV{PERL5LIB} = $ENV{PERL5LIB}
-        ? do { require Config; $self->lib . $Config::Config{path_sep} . $ENV{PERL5LIB} }
-        : $self->lib
-        ;
+        ? do {
+        require Config;
+        $self->lib . $Config::Config{path_sep} . $ENV{PERL5LIB};
+        }
+        : $self->lib;
     return 1;
 }
 
-sub root            { shift->{root} }
-sub config_file     { shift->{config_file} }
-sub lib             { shift->{lib} }
+sub root        { return shift->{root} }
+sub config_file { return shift->{config_file} }
+sub lib         { return shift->{lib} }
+
 sub config {
     my $self = shift;
-    croak "no config file available"
-        unless $self->{config_file};
+    croak 'no config file available'
+        if !$self->{config_file};
     return $self->{config} ||= do {
         require Config::JSON;
-        Config::JSON->new($self->config_file);
+        Config::JSON->new( $self->config_file );
     };
 }
 
 sub config_file_relative {
     my $self = shift;
     return $self->{config_file_relative} ||= do {
-        my $config_dir = Cwd::realpath(File::Spec->catdir($self->root, 'etc'));
-        File::Spec->abs2rel(Cwd::realpath($self->config_file), $config_dir);
-    }
+        my $config_dir
+            = Cwd::realpath( File::Spec->catdir( $self->root, 'etc' ) );
+        File::Spec->abs2rel( Cwd::realpath( $self->config_file ),
+            $config_dir );
+    };
 }
 
 sub db {
     my $self = shift;
     require WGDev::Database;
-    return $self->{db} ||= WGDev::Database->new($self->config);
+    return $self->{db} ||= WGDev::Database->new( $self->config );
 }
 
 sub session {
     my $self = shift;
     require WebGUI::Session;
-    if ($self->{session}) {
+    if ( $self->{session} ) {
         my $dbh = $self->{session}->db->dbh;
+
         # evil, but we have to detect if the database handle died somehow
-        eval {
-            local $dbh->{PrintWarn} = 0;
-            local $dbh->{PrintError} = 0;
-            local $dbh->{RaiseError} = 1;
-            $dbh->do('SELECT 1');
-        };
-        if ($@) {
-            (delete $self->{session})->close;
+        if (
+            !eval {
+                ## no critic (ProhibitLocalVars)
+                local $dbh->{PrintWarn}  = 0;
+                local $dbh->{PrintError} = 0;
+                local $dbh->{RaiseError} = 1;
+                $dbh->do('SELECT 1');
+            } )
+        {
+            ( delete $self->{session} )->close;
         }
     }
     return $self->{session} ||= do {
-        my $session = WebGUI::Session->open($self->root, $self->config_file_relative, undef, undef, $self->{session_id});
-        # eeevil
+        my $session
+            = WebGUI::Session->open( $self->root, $self->config_file_relative,
+            undef, undef, $self->{session_id} );
         $self->{session_id} = $session->getId;
         $session;
     };
@@ -128,23 +146,25 @@ sub session {
 sub asset {
     my $self = shift;
     require WGDev::Asset;
-    $self->{asset} ||= WGDev::Asset->new($self->session);
+    return $self->{asset} ||= WGDev::Asset->new( $self->session );
 }
 
 sub version {
     my $self = shift;
     require WGDev::Version;
-    $self->{version} ||= WGDev::Version->new($self->root);
+    return $self->{version} ||= WGDev::Version->new( $self->root );
 }
 
 sub DESTROY {
     my $self = shift;
-    if ($self->{session}) {
-        my $session = $self->session;
-        $session->var->end;
+
+    if ( $self->{session} ) {    # if we have a cached session
+        my $session = $self->session;  # get the session, recreating if needed
+        $session->var->end;            # close the session
         $session->close;
         delete $self->{session};
     }
+    return;
 }
 
 1;
@@ -155,6 +175,15 @@ __END__
 
 WGDev - WebGUI Developer Utilities
 
+=head1 SYNOPSIS
+
+    use WGDev;
+
+    my $wgd = WGDev->new( $webgui_root, $config_file );
+
+    my $webgui_session = $wgd->session;
+    my $webgui_version = $wgd->version->module;
+
 =head1 DESCRIPTION
 
 Performs common actions needed by WebGUI developers, such as recreating their
@@ -164,11 +193,13 @@ site from defaults, checking version numbers, exporting packages, and more.
 
 Graham Knop <graham@plainblack.com>
 
-=head1 COPYRIGHT
+=head1 LICENSE
 
 Copyright (c) 2008 Graham Knop.  All rights reserved.
 
 This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
 =cut
+
+
 
