c6b56604b8270dc0fa541e3a6d3eb39504fce83c
Graham Knop <haarg@haarg.org>
2008-12-24 18:37:17 -0600

    convert Test command to use WGDev::Command::Base

--- lib/WGDev/Command/Test.pm
+++ lib/WGDev/Command/Test.pm
@@ -4,17 +4,17 @@ use warnings;
 
 our $VERSION = '0.0.1';
 
-sub run {
-    require App::Prove;
+use WGDev::Command::Base;
+our @ISA = qw(WGDev::Command::Base);
+
+use File::Spec;
+
+sub process {
     require Cwd;
-    my $class = shift;
-    my $wgd = shift;
-    Getopt::Long::Configure(qw(default gnu_getopt pass_through));
-    Getopt::Long::GetOptionsFromArray(\@_,
-        'S'     => \(my $opt_slow),
-        'A'     => \(my $opt_all),
-    );
-    if ($opt_slow) {
+    require App::Prove;
+    my $self = shift;
+    my $wgd = $self->wgd;
+    if ($self->option('slow')) {
         $ENV{CODE_COP}      = 1;
         $ENV{TEST_SYNTAX}   = 1;
         $ENV{TEST_POD}      = 1;
@@ -22,10 +22,10 @@ sub run {
     my $prove = App::Prove->new;
     my @args = @_;
     my $orig_dir;
-    if ($opt_all) {
-        $orig_dir = Cwd::cwd;
+    if ($self->option('slow')) {
+        $orig_dir = Cwd::cwd();
         chdir File::Spec->catdir($wgd->root, 't');
-        push @args, '-r';
+        unshift @args, '-r';
     }
     $prove->process_args(@args);
     my $result = $prove->run;
@@ -34,12 +34,20 @@ sub run {
     return $result;
 }
 
-sub usage {
-    my $class = shift;
-    return <<END_HELP;
-this command is not very helpful
-END_HELP
-}
+sub option_parse_config { qw(gnu_getopt pass_through) }
+
+sub option_config {qw(
+    all|A
+    slow|S
+)}
 
 1;
+
+__END__
+
+=head1 NAME
+
+WGDev::Command::Test - Does things
+
+=cut
 
