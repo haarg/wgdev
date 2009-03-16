f59076f417414f8c83cbaa1d5d63b3a0dcf7c447
Graham Knop <haarg@haarg.org>
2009-03-16 16:16:32 -0500

    perlcritic on optimize command

--- lib/WGDev/Command/Optimize.pm
+++ lib/WGDev/Command/Optimize.pm
@@ -18,31 +18,39 @@ sub option_config {
 sub process {
     my $self = shift;
     my $wgd  = $self->wgd;
-    
-    if ($self->option('assets')) {
+
+    if ( $self->option('assets') ) {
         $self->optimise_assets();
     }
 
-    if ($self->option('macros')) {
+    if ( $self->option('macros') ) {
         $self->optimise_macros();
     }
+    return 1;
 }
 
 sub optimise_assets {
-    my $self = shift;
-    my $wgd  = $self->wgd;
+    my $self    = shift;
+    my $wgd     = $self->wgd;
     my $session = $wgd->session();
-    
+
     my @assets;
     for my $asset ( sort keys %{ $session->config->get('assets') } ) {
-        push @assets, $asset
-            unless $session->db->quickScalar('select count(*) from asset where className = ?', [$asset]);
+        if (
+            !$session->db->quickScalar(
+                'select count(*) from asset where className = ?', [$asset] ) )
+        {
+            push @assets, $asset;
+        }
     }
-    
+
     if (@assets) {
-        $self->report("The following Assets do not appear in your Asset table:\n");
-        $self->report("\t$_\n") for @assets;
-        my $config = $wgd->config_file();
+        $self->report(
+            "The following Assets do not appear in your Asset table:\n");
+        for my $asset (@assets) {
+            $self->report("\t$asset\n");
+        }
+        my $config  = $wgd->config_file();
         my $message = <<"END_MESSAGE";
 If you are sure any of these Assets are not being used on your site,
 you can reduce memory usage by removing them from the "assets" section of
@@ -61,21 +69,28 @@ END_MESSAGE
 }
 
 sub optimise_macros {
-    my $self = shift;
-    my $wgd  = $self->wgd;
+    my $self    = shift;
+    my $wgd     = $self->wgd;
     my $session = $wgd->session();
-    
+
     my @macros;
     for my $macro ( sort keys %{ $session->config->get('macros') } ) {
-        push @macros, $macro
-            unless $session->db->quickScalar("select count(*) from template where template like ? or template like ?", ["%^$macro;%", "%^$macro(%"]);
+        if (
+            !$session->db->quickScalar(
+                'select count(*) from template where template like ? or template like ?',
+                [ "%^$macro;%", "%^$macro(%" ] ) )
+        {
+            push @macros, $macro;
+        }
     }
-    
+
     if (@macros) {
-        $self->report("The following Macros do not appear in the template field of the template table:\n");
-        $self->report("\t$_\n") for @macros;
-        my $config = $wgd->config_file();
+        my $macros  = join q{}, map {"\t$_\n"} @macros;
+        my $config  = $wgd->config_file();
         my $message = <<"END_MESSAGE";
+The following Macros do not appear in the template field of the template table:
+$macros
+
 If you are sure any of these Macros are not being used on your site,
 you can reduce memory usage by removing them from the "macros" section of
 your site config file, which is located at:
@@ -85,7 +100,7 @@ Keep in mind:
 *) Macros can be references from lots of places other then just Templates, 
    for example the mailFooter setting in the Settings table 
 END_MESSAGE
-        $self->report("$message");
+        $self->report($message);
     }
 
     return 1;
