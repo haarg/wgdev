af752dcd2cea2f9d5141f8ee116f452c449f7e00
Graham Knop <haarg@haarg.org>
2009-07-17 18:05:13 -0500

    adjustments to ls command filters due to obsessive stylistic concerns

--- lib/WGDev/Command/Ls.pm
+++ lib/WGDev/Command/Ls.pm
@@ -21,9 +21,31 @@ sub config_options {
     );
 }
 
-sub process {
+sub option_filter {
     my $self   = shift;
-    my $wgd    = $self->wgd;
+    my $filter = shift;
+
+    my ( $filter_prop, $filter_match )
+        = $filter =~ m{%(\w+)% \s* ~~ \s* (.*)}msx;
+    if ( !defined $filter_prop || !defined $filter_match ) {
+        die "Invalid filter specified!\n";
+    }
+    if ( $filter_match =~ m{\A/(.*)/\Z}msx ) {
+        eval { $filter_match = qr/$1/msx; }
+            || die "Invalid filter regex specified: $1\n";
+    }
+    else {
+        $filter_match = qr/\A\Q$filter_match\E\z/msx;
+    }
+    $self->{filter_property} = $filter_prop;
+    $self->{filter_match}    = $filter_match;
+    return;
+}
+
+sub process {
+    my $self = shift;
+    my $wgd  = $self->wgd;
+
     my $format = $self->option('format');
     if ( $self->option('long') ) {
         $format = '%assetId% %url:-35% %title%';
@@ -31,24 +53,15 @@ sub process {
     elsif ( !$format ) {
         $format = '%url%';
     }
-    my $relatives = $self->option('recursive') ? 'descendants' : 'children';
+
+    my $relatives   = $self->option('recursive') ? 'descendants' : 'children';
     my @parents     = $self->arguments;
     my $show_header = @parents > 1;
-    my $excludeClasses = $self->option('excludeClass');
-    my $includeOnlyClasses = $self->option('includeOnlyClass');
-    my $limit = $self->option('limit');
-    my $isa = $self->option('isa');
-    my ($filter_prop, $filter_smartmatch);
-    if (my $filter = $self->option('filter')) {
-        # Try matching filter_smartmatch as a regex
-        ($filter_prop, $filter_smartmatch) = $filter =~ m{%(\w+)% \s* ~~ \s* /(.*)/}x;
-        if (defined $filter_smartmatch) {
-            $filter_smartmatch = eval { qr/$filter_smartmatch/ };
-        } else {
-            # otherwise, match filter_smatch as a simple string
-            ($filter_prop, $filter_smartmatch) = $filter =~ m{%(\w+)% \s* ~~ \s* (.*)}x;
-        }
-    }
+    my $exclude_classes      = $self->option('excludeClass');
+    my $include_only_classes = $self->option('includeOnlyClass');
+    my $limit                = $self->option('limit');
+    my $isa                  = $self->option('isa');
+
     PARENT:
     while ( my $parent = shift @parents ) {
         my $asset;
@@ -59,42 +72,29 @@ sub process {
         if ($show_header) {
             print "$parent:\n";
         }
-        my $children = $asset->getLineage(
+        my $child_iter = $asset->getLineageIterator(
             [$relatives],
-            {   returnObjects => 1,
-                $excludeClasses     ? ( excludeClasses     => $excludeClasses )     : (),
-                $includeOnlyClasses ? ( includeOnlyClasses => $includeOnlyClasses ) : (),
-                defined $limit && !defined $filter_smartmatch ? ( limit => $limit ) : (),
+            {
+                $exclude_classes ? ( excludeClasses => $exclude_classes )
+                : (),
+                $include_only_classes
+                ? ( includeOnlyClasses => $include_only_classes )
+                : (),
+                defined $limit
+                    && !defined $self->{filter_match} ? ( limit => $limit )
+                : (),
                 $isa ? ( isa => $isa ) : (),
-            }
-        );
-        for my $child ( @{$children} ) {
-            if (defined $filter_smartmatch) {
-                # N.B. When we require perl 5.10 this can use ~~ for both cases
-                if (ref $filter_smartmatch eq 'Regexp') {
-                    next unless $child->get($filter_prop) =~ $filter_smartmatch;
-                } else {
-                    next unless $child->get($filter_prop) eq $filter_smartmatch;
-                }
-                
-                # Handle limit ourselves when filtering because filtering happens
-                # *after* getLineage returns its results
-                last PARENT if defined $limit && $limit-- <= 0;
-            }
-            my $output = $format;
-            $output =~ s{% (?: (\w+) (?: :(-?\d+) )? )? %}{
-                my $replace;
-                if ($1) {
-                    $replace = $child->get($1);
-                    if ($2) {
-                        $replace = sprintf("%$2s", $replace);
-                    }
-                }
-                else {
-                    $replace = '%';
-                }
-                $replace;
-            }msxeg;
+            } );
+        while ( my $child = $child_iter->() ) {
+            next
+                if !$self->pass_filter($child);
+
+            # Handle limit ourselves because smartmatch filtering happens
+            # *after* getLineage returns its results
+            last PARENT
+                if defined $limit && $limit-- <= 0;
+
+            my $output = $self->format_output( $format, $child );
             print $output . "\n";
         }
         if (@parents) {
@@ -102,6 +102,35 @@ sub process {
         }
     }
     return 1;
+}
+
+sub pass_filter {
+    my ( $self, $asset ) = @_;
+    my $filter_prop  = $self->{filter_property};
+    my $filter_match = $self->{filter_match};
+
+    return 1
+        if !defined $filter_match;
+
+    return $asset->get($filter_prop) =~ $filter_match;
+}
+
+sub format_output {
+    my ( $self, $format, $asset ) = @_;
+    $format =~ s{% (?: (\w+) (?: :(-?\d+) )? )? %}{
+        my $replace;
+        if ($1) {
+            $replace = $asset->get($1);
+            if ($2) {
+                $replace = sprintf("%$2s", $replace);
+            }
+        }
+        else {
+            $replace = '%';
+        }
+        $replace;
+    }msxeg;
+    return $format;
 }
 
 1;
@@ -135,7 +164,7 @@ the field to display, and 30 is the length to left pad/cut to.  Negative
 lengths can be specified for right padding.  Percent signs can be included by
 using C<%%>.
 
-=item C<--recursive=> C<-r>
+=item C<--recursive> C<-r>
 
 Recursively list all descendants (by default we only list children).
 
@@ -153,11 +182,11 @@ The maximum amount of entries to return
 
 =item C<--isa=>
 
-A classname where you can look for classes of a similar base class. For example, if you're looking for Donations, Subscriptions, Products and other subclasses of WebGUI::Asset::Sku, then set isa to 'WebGUI::Asset::Sku'.
+A class name where you can look for classes of a similar base class. For example, if you're looking for Donations, Subscriptions, Products and other subclasses of L<WebGUI::Asset::Sku>, then specify the parameter C<--isa=WebGUI::Asset::Sku>.
 
 =item C<--filter=>
 
-Apply smartmatch filtering against the results. Format looks like C<%url% ~~ smartmatch>, where C<url> is
+Apply smart match filtering against the results. Format looks like C<%url% ~~ smartmatch>, where C<url> is
 the field to filter against, and C<smartmatch> is either a Perl regular expression such as C</(?i:partial_match)/> or
 a string such as C<my_exact_match>.
 
@@ -169,7 +198,7 @@ Graham Knop <graham@plainblack.com>
 
 =head1 LICENSE
 
-Copyright (c) Graham Knop.  All rights reserved.
+Copyright (c) Graham Knop.
 
 This library is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
