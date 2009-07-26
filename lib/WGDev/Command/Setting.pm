cffb4b9c1d1cce18f3676433af5b0c98bd417626
Colin Kuskie <colink@perldreamer.com>
2009-07-26 17:46:48 -0500

    Adding Setting command

---
+++ lib/WGDev/Command/Setting.pm
@@ -0,0 +1,65 @@
+package WGDev::Command::Setting;
+use strict;
+use warnings;
+use 5.008008;
+
+our $VERSION = '0.2.0';
+
+use WGDev::Command::Base;
+BEGIN { our @ISA = qw(WGDev::Command::Base) }
+
+sub config_options {
+    return qw();
+}
+
+sub process {
+    my $self = shift;
+    my $wgd  = $self->wgd;
+    my $session = $wgd->session;
+    foreach my $setting ( $self->arguments ) {
+        print sprintf "%s:%s\n", $setting, $session->setting->get($setting);
+    }
+
+    return 1;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+WGDev::Command::Setting - Returns WebGUI settings from the database.
+
+=head1 SYNOPSIS
+
+    wgd setting shopMySalesTemplateId shopStyleTemplateId
+
+=head1 DESCRIPTION
+
+Prints settings from the WebGUI settings table.  This is handy for doing quick lookups,
+or for using as part of other wgd commands.
+
+=head1 OPTIONS
+
+No options
+
+=back
+
+=head1 METHODS
+
+No methods
+
+=head1 AUTHOR
+
+Colin Kuskie <colink@perldreamer.com>
+
+=head1 LICENSE
+
+Copyright (c) Graham Knop.  All rights reserved.
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
