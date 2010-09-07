90070290d1b7aaf463262d37f5e80780c045a8dc
Graham Knop <haarg@haarg.org>
2010-09-07 13:46:39 -0500

    fix example for Dist::Zilla

--- ex/Command.pm
+++ ex/Command.pm
@@ -1,9 +1,8 @@
 package WGDev::Command::Example;
+# ABSTRACT: Example WGDev Command
 use strict;
 use warnings;
 use 5.008008;
-
-our $VERSION = '0.0.1';
 
 use WGDev::Command::Base;
 BEGIN { our @ISA = qw(WGDev::Command::Base) }
@@ -28,12 +27,6 @@ sub process {
 
 1;
 
-__DATA__
-
-=head1 NAME
-
-WGDev::Command::Example - Example WGDev Command
-
 =head1 SYNOPSIS
 
     wgd example [-A]
@@ -51,18 +44,6 @@ This is a sample command.
 Does everything.
 
 =back
-
-=head1 AUTHOR
-
-Graham Knop <haarg@haarg.org>
-
-=head1 LICENSE
-
-Copyright (c) 2009-2010, Graham Knop
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl 5.10.0. For more details, see the
-full text of the licenses in the directory LICENSES.
 
 =cut
 
